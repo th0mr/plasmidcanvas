e5a21a79e83dcd6e5d1ef3954ff97569ef7bafa3
th0mr <87852659+th0mr@users.noreply.github.com>
2024-01-22 17:01:53 +0000

    Markers done - adding SinglePairLabel

--- src/plasmidcanvas/feature.py
+++ src/plasmidcanvas/feature.py
@@ -1,5 +1,6 @@
 from matplotlib.axes import Axes
-from matplotlib.patches import Polygon, Wedge
+from matplotlib.patches import FancyArrowPatch, Polygon, Wedge
+import numpy as np
 
 # ==================================
 # Abstract Feature Classes
@@ -46,17 +47,50 @@ class MultiPairFeature(Feature):
 
 class SinglePairFeature(Feature):
 
+    base_pair: int
+
     def __init__(self) -> None:
         super().__init__()
+        
+    def get_base_pair(self) -> int:
+        return self.base_pair
+
+    def set_base_pair(self, base_pair) -> None:
+        self.base_pair = base_pair
 
 # ==================================
 # Concrete Feature Classes
+
+class SinglePairLabel(SinglePairFeature):
+    
+    DEFAULT_LINE_LENGTH_SF = 1.1
+    
+    def __init__(self) -> None:
+        super().__init__()
+        
+    def render(self, ax: Axes, p_total_base_pairs: int, p_center: tuple[float, float], p_radius: float, p_line_width: float) -> None:
+        
+        # IN PROGRESS!!!!
+        
+        # Calculate the x and y of the outer coordinates of the line
+        x = (p_radius + p_line_width * self.DEFAULT_LINE_LENGTH_SF) * np.cos(angles)
+        y = (p_radius + p_line_width * self.DEFAULT_LINE_LENGTH_SF) * np.sin(angles)
+        
+        ax.plot()
+
+# Currently just an alias for a SinglePairLabel
+class RestrictionSite(SinglePairLabel):
+    
+    def __init__(self) -> None:
+        super().__init__()
 
 class RectangleFeature(MultiPairFeature):
 
     # Center and radius for plotting the curved rectangle against plasmid circle
     center: tuple[int]
     radius: float
+    
+    line_width_scale_factor: float = 1
 
     def render(self, ax: Axes, p_total_base_pairs: int, p_center: tuple[float, float], p_radius: float, p_line_width: float) -> None:
         # 1 - Work out angles from 12 o clock
@@ -85,26 +119,76 @@ class RectangleFeature(MultiPairFeature):
         # TODO - Support on / off the circle placement by moving radius
 
         # 4 - Place the wedge
-        rectangle = Wedge(center=p_center, r=r_radius, theta1=theta_1, theta2=theta_2, width=p_line_width)
+        rectangle = Wedge(center=p_center, r=r_radius, theta1=theta_1, theta2=theta_2, width=(p_line_width * self.line_width_scale_factor), label="blah testing")
         ax.add_patch(rectangle)
-
+    
+    def set_line_width_scale_factor(self, sf: float) -> None:
+        self.line_width_scale_factor = sf
+        
 
 class ArrowFeature(RectangleFeature):
 
     def render(self, ax: Axes, p_total_base_pairs: int, p_center: tuple[float, float], p_radius: float, p_line_width: float) -> None:
         super().render(ax, p_total_base_pairs, p_center, p_radius, p_line_width)
-        # Triangle edges
-        offset = p_line_width * 2
-        xcent  = p_center[0] - p_radius + (p_line_width*2)
-        print(f"xcent={xcent}")
-        left   = [xcent - offset, p_center[1]]
-        print(f"left={left}")
-        right  = [xcent + offset, p_center[1]]
-        print(f"right={right}")
-        bottom = [(left[0]+right[0])/2., p_center[1]-100]
-        print(f"bottom={bottom}")
-        arrow  = Polygon([left, right, bottom, left])
-        ax.add_patch(arrow)
+        # # # Triangle edges
+        # # offset = p_line_width * 2
+        # # xcent  = p_center[0] - p_radius + (p_line_width*2)
+        # # print(f"xcent={xcent}")
+        # # left   = [xcent - offset, p_center[1]]
+        # # print(f"left={left}")
+        # # right  = [xcent + offset, p_center[1]]
+        # # print(f"right={right}")
+        # # bottom = [(left[0]+right[0])/2., p_center[1]-100]
+        # # print(f"bottom={bottom}")
+        # # arrow  = Polygon([left, right, bottom, left])
+        # # ax.add_patch(arrow)
+        
+        # # 1 - Work out angles from 12 o clock
+        # # theta1 is the angle from the start of the plasmid (0th bp) to the start of the plasmid
+        # twelve_to_start_angle = (self.get_start_pairs() / p_total_base_pairs) * 360
+        # print(f"twelve_to_start_angle={twelve_to_start_angle}")
+        # # theta2 is the angle from the start of the feature to the end of the feature
+        # start_to_end_angle = twelve_to_start_angle + ((self.length() / p_total_base_pairs) * 360)
+        # print(f"start_to_end_angle={start_to_end_angle}")
+        
+        # angle_radians_theta_1 = np.radians(twelve_to_start_angle)
+        # angle_radians_theta_2 = np.radians(start_to_end_angle)
+        # xy_theta_1 = (p_radius * np.sin(angle_radians_theta_1), p_radius * np.cos(angle_radians_theta_1))
+        # print(f"xy_theta_1={xy_theta_1}")
+        # xy_theta_2 = (p_radius * np.sin(angle_radians_theta_2), p_radius * np.cos(angle_radians_theta_2))
+        # print(f"xy_theta_2={xy_theta_2}")
+        
+        # # 3 - TODO - Adjust placement of the rectangle
+        # # For the rectangle to sit on top of the plasmid ring, the radius must be beyond 
+        # # the radius of the plasmid + half the plasmid circle width
+        # r_radius: float = p_radius
+        
+        # # rwidth is the width of the rectangle, calculated by adding a scale factor to the width of the plasmid line
+        # r_width: float = p_radius
+        
+        # # rectangle = Wedge(center=p_center, r=r_radius, theta1=theta_1, theta2=theta_2, width=p_line_width)
+        # # ax.add_patch(rectangle)
+
+        # # TODO - Support on / off the circle placement by moving radius
+        # style = "Simple, tail_width=0.5, head_width=4, head_length=8"
+        # kw = dict(arrowstyle=style, color="k")
+
+        # arrow_patch: FancyArrowPatch = FancyArrowPatch(xy_theta_1,xy_theta_2, connectionstyle=f"arc3,rad=0.5", **kw)
+        # ax.add_patch(arrow_patch)
+
+        # # 4 - Place the wedge
+        # # Triangle edges
+        # # offset = p_line_width * 2
+        # # xcent  = p_center[0] - p_radius + (p_line_width*2)
+        # # print(f"xcent={xcent}")
+        # # left   = [xy_theta_2[0], p_center[1]]
+        # # print(f"left={left}")
+        # # right  = [xcent + offset, p_center[1]]
+        # # print(f"right={right}")
+        # # bottom = [(left[0]+right[0])/2., p_center[1]-100]
+        # # print(f"bottom={bottom}")
+        # # arrow: Polygon  = Polygon([left, right, bottom, left])
+        # # ax.add_patch(arrow)
 
 
 class CircleFeature(MultiPairFeature):
--- src/plasmidcanvas/plasmid.py
+++ src/plasmidcanvas/plasmid.py
@@ -5,15 +5,20 @@ from math import pi
 from matplotlib.axes import Axes
 from matplotlib.figure import Figure
 from matplotlib.patches import Circle, Wedge
+import numpy as np
 
 from feature import Feature, RectangleFeature, ArrowFeature
+
+
 
 class Plasmid:
 
     DEFAULT_CIRCLE_RADIUS: float = 1000
     DEFAULT_CIRCLE_CENTER: tuple[float, float] = (0,0)
-    DEFAULT_PLASMID_LINE_WIDTH: float = DEFAULT_CIRCLE_RADIUS * 0.05
+    DEFAULT_PLASMID_LINE_WIDTH: float = DEFAULT_CIRCLE_RADIUS * 0.10
     DEFAULT_PLASMID_NAME: str = "Untitled Plasmid"
+    DEFAULT_PLASMID_NUMBER_OF_MARKERS: int = 16
+    DEFAULT_MARKER_DISTANCE_SF = 1.10
 
     name: str = DEFAULT_PLASMID_NAME
     base_pairs: int = 0
@@ -25,6 +30,12 @@ class Plasmid:
     def __init__(self, name: str, base_pairs: int) -> None:
         self.set_base_pairs(base_pairs)
         self.set_name(name)
+
+    def degrees_to_basepair(self, degree: float) -> int:
+        return round((degree / 360) * self.base_pairs)
+
+    def basepair_to_degrees(self, basepair: int) -> float:
+        return (basepair / self.base_pairs) * 360
 
     def plot(self) -> None:
         fig: Figure
@@ -40,16 +51,20 @@ class Plasmid:
 
         ax.set_xlim((-self.DEFAULT_CIRCLE_RADIUS * 1.5, self.DEFAULT_CIRCLE_RADIUS * 1.5))
         ax.set_ylim((-self.DEFAULT_CIRCLE_RADIUS * 1.5, self.DEFAULT_CIRCLE_RADIUS * 1.5))
-
         
+        # Assemble a set of points to plot label markers around
+        degrees_to_place_markers = np.linspace(0, 360, self.DEFAULT_PLASMID_NUMBER_OF_MARKERS, endpoint=False)
+        for degree in degrees_to_place_markers:
+            x,y = (self.DEFAULT_CIRCLE_CENTER[0] + self.DEFAULT_CIRCLE_RADIUS * self.DEFAULT_MARKER_DISTANCE_SF * np.sin(np.deg2rad(degree)), 
+                   self.DEFAULT_CIRCLE_CENTER[1] + self.DEFAULT_CIRCLE_RADIUS * self.DEFAULT_MARKER_DISTANCE_SF * np.cos(np.deg2rad(degree)))
+            ax.text(x,y, s=f"{self.degrees_to_basepair(degree)}", horizontalalignment='center', fontstyle='italic', alpha=0.5, fontsize=7)
 
         # Add all features to the plasmid map by running their render() method
         for feature in self.get_features():
-            feature.render(ax, self.get_base_pairs(), (0,0), self.DEFAULT_CIRCLE_RADIUS, self.DEFAULT_PLASMID_LINE_WIDTH)
+            feature.render(ax, self.get_base_pairs(), self.DEFAULT_CIRCLE_CENTER, self.DEFAULT_CIRCLE_RADIUS, self.DEFAULT_PLASMID_LINE_WIDTH)
 
     def save_to_file(self, fig: Figure, filename: str) -> None:
         pass
-
 
     def render(self, ax: Axes) -> None:
         center: tuple[float, float] = self.DEFAULT_CIRCLE_CENTER
@@ -104,14 +119,13 @@ class PlasmidStyle:
         pass
 
 
-plasmid = Plasmid("MyPlasmid", 500)
+plasmid = Plasmid("MyPlasmid", 720)
 
 first_rect = RectangleFeature(100,200)
+first_rect.set_line_width_scale_factor(1)
 first_arrow = ArrowFeature(300,350)
-second_arrow = ArrowFeature(400,500)
 
 plasmid.add_feature(first_rect)
 plasmid.add_feature(first_arrow)
-plasmid.add_feature(second_arrow)
 
 plasmid.plot()
