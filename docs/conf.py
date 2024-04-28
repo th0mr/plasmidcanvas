fb4c86e1bfe72f11fd6a3dbc25ef813047254673
th0mr <thomrobinson76@gmail.com>
2024-04-28 17:08:02 +0100

    correct conf.py version number

--- docs/conf.py
+++ docs/conf.py
@@ -9,7 +9,7 @@
 project = 'plasmidcanvas'
 copyright = '2024, Thom Robinson'
 author = 'Thom Robinson'
-release = '0.1.0'
+release = '1.0.0'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
